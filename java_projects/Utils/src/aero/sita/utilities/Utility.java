cb07a9b1e00b425db21799d23b63e1101e4645ea
Anand Kumar <anandk2@hexaware.com>
2018-09-20 13:33:08 +0530

    JAVA code updated Utils.jar

--- java_projects/Utils/src/aero/sita/utilities/Utility.java
+++ java_projects/Utils/src/aero/sita/utilities/Utility.java
@@ -4,7 +4,18 @@ import java.util.Base64;
 
 public class Utility {
 
+	public static String encodeBase64(byte[] inputByteArr) {
+		System.out.println("encodeBase64 -> "+inputByteArr);
+		return Base64.getEncoder().encodeToString(inputByteArr);
+	}
+	
 	public static String encodeBase64(String inputStr) {
+		System.out.println("encodeBase64 -> "+inputStr);
 		return Base64.getEncoder().encodeToString(inputStr.getBytes());
+	}
+	
+	public static String byteArrToString(byte[] inputByteArr) {
+		System.out.println("byteArrToString -> "+inputByteArr);
+		return new String(inputByteArr);
 	}
 }
